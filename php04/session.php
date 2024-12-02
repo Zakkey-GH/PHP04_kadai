db914fa3b3a4c5d676a97521b06a1ad856fffefa
Zakkey-GH <reedinortoop@gmail.com>
2024-12-03 05:01:52 +0900

    3?

--- php04/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-$_SESSION["name"]="yamazaki";
-$_SESSION["age"] = 20;
-
-echo session_id();
--- php04/session2.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); //KEY発行：ユニーク
-
-echo $_SESSION["name"];
-echo "<br>";
-echo session_id(); //KEY確認用
